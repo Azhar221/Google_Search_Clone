b3015508cd37661507c57b1a0e52cb3167b944eb
Sayyad Azhar <80194496+Azhar221@users.noreply.github.com>
2022-03-05 11:52:49 +0530

    Removeing unwanted files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>azhar</h1>
+            <div className='home_header'>
+
+                <div className='home_headerLeft'>
+
+                </div>
+                <div className='home_headerRight'>
+
+                </div>
+            </div>
+            <div className='home_body'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
